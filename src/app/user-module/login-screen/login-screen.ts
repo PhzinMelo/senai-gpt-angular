acf977c1fa515421766626a9d9f5ca524b8b5d63
Pedro Melo <pedrohenriquemelo1010@gmail.com>
2025-09-03 21:58:15 -0300

    Bind do campo de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
-
+  loginForm: FormGroup;
+  constructor(private fb:FormBuilder) {  
+    //inicia o formulario
+    //Cria o campo obrigatorio de email.
+    //Cria o campo obrigatorio da senha.
+    this.loginForm=this.fb.group({
+      email: ["",[Validators.required]],
+      password: ["",[Validators.required]]
+    })
+  }
+  onLoginClick(){ 
+    alert("Botão de login clicado")
+    console.log("Email",this.loginForm.value.email)
+    console.log("Password",this.loginForm.value.password)
+  }
 }
