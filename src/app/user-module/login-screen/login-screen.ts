8e3d7c5db7e352cb55136a730389e8177a508b0e
Pedro Melo <pedrohenriquemelo1010@gmail.com>
2025-09-04 21:57:13 -0300

    Validação de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -18,9 +18,48 @@ export class LoginScreen {
       password: ["",[Validators.required]]
     })
   }
-  onLoginClick(){ 
+  async onLoginClick(){ 
     alert("Botão de login clicado")
     console.log("Email",this.loginForm.value.email)
     console.log("Password",this.loginForm.value.password)
+    
+    if(this.loginForm.value.email ==""){  
+      alert("Preencha o email")
+    return; }
+
+    if (this.loginForm.value.password =="") {
+      alert("Preencha a senha")
+    return;
+    }
+    
+    let response =await fetch("https://senai-gpt-api.azurewebsites.net/login", {  
+      method: "Post",
+      headers: {  
+        "Content-Type" : "application/json"
+      },body: JSON.stringify({ 
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+   
+    
+    })
+     
+    console.log("Status code" + response.status);
+
+    if (response.status>=200 && response.status<=299) {  
+      alert("Deu bom paizao")
+    }
+    else  {  
+      alert("Deu ruim paizao")
+    }
+    
+    let email2 =this.loginForm.value.email ;
+    let password2=this.loginForm.value.password ;
+    if (email2.lenght ===0) {
+      alert('Campo de email obrigatorio') }
+    else if(password2.lenght ===0){
+      alert(`Campo de senha obrigatorio`)}
+      
+    
   }
 }
