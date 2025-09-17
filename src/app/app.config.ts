526739e627d1c1b6427717e7a644e9bb0a1479f7
Pedro Melo <pedrohenriquemelo1010@gmail.com>
2025-09-17 19:39:53 -0300

    consertando algumas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+export const authInterceptor=(req,next)=> { 
 
+  const router= inject(Router);
+  return next(req).pipe(  
+    catchError((err: HttpErrorResponse) => { 
+      if(err.status===401){ 
+        localStorage.clear
+        router.navigate(['/login'])
+      }
+      return throwError(() =>err)
+    })
+  );
+}
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([])
+    ),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
